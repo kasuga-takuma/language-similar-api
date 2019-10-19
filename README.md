46a0bacdf35f37d19ae9d30dd6122fabd2fe44dd
kasuga <kasuga@excitent.onmicrosoft.com>
2019-10-19 17:26:11 +0900

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,31 @@
 # language-similar-api
+## URL
+https://sentence-similarity-api.herokuapp.com/
+## 実行準備
+モデルの用意
+## 開発環境
+* python3.6.9
+* UbuntuOS(GoogleColaboratory)  
+※WindowsOSで実行時には環境構築時に以下の変更が必要
+```text: requirements
+mecab-python3==0.996.2
+```
+を
+```text: requirements
+mecab-python-windows=0.996.3
+```
+に変更  
+## 送受信データ形式
+* request(POSTのみ)
+```
+{
+  'sentence1': '<文章1>',
+  'sentence2': '<文章2>'
+}
+```
+* response
+```
+{
+  'similarity': <類似度>
+}
+```
---
+++ view.py
@@ -0,0 +1,45 @@
+# coding: utf-8
+
+from flask import Flask, request
+import MeCab
+import gensim
+import numpy as np
+
+app = Flask(__name__)
+mt = MeCab.Tagger()
+mt.parse('')
+model = gensim.models.Word2Vec.load('./ja/ja.bin')
+
+@app.route('/', methods=['POST'])
+def api():
+    json = request.get_json()  # Get POST JSON
+    print(json)
+    vector1 = json['sentence1']
+    vector2 = json['sentence2']
+    v1 = get_vector(vector1)
+    v2 = get_vector(vector2)
+    return {'similarity': cos_sim(v1, v2)}
+
+def get_vector(text):
+    sum_vec = np.zeros(300)
+    word_count = 0
+    node = mt.parseToNode(text)
+    while node:
+        fields = node.feature.split(",")
+        # 名詞、動詞、形容詞に限定
+        if fields[0] == '名詞' or fields[0] == '動詞' or fields[0] == '形容詞' or fields[0] == '副詞':
+          try:
+            sum_vec += model.wv[node.surface]
+            word_count += 1
+          except KeyError:
+            word_count += 1
+        node = node.next
+    return sum_vec / word_count
+
+# cos類似度を計算
+def cos_sim(v1, v2):
+    return np.dot(v1, v2) / (np.linalg.norm(v1) * np.linalg.norm(v2))
+
+# メイン関数
+if __name__ == '__main__':
+    app.run(debug=True)
