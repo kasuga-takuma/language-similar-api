253e32768973964b8358c4c9c12f26f34aa9a9e3
kasuga-takuma <kasuga_takuma@tsolweb.co.jp>
2019-12-06 10:08:40 +0900

    added put method for test

--- view.py
+++ view.py
@@ -10,15 +10,24 @@ mt = MeCab.Tagger()
 mt.parse('')
 model = gensim.models.Word2Vec.load('./ja/ja.bin')
 
+
 @app.route('/', methods=['POST'])
 def api():
     json = request.get_json()  # Get POST JSON
-    print(json)
     vector1 = json['sentence1']
     vector2 = json['sentence2']
     v1 = get_vector(vector1)
     v2 = get_vector(vector2)
     return {'similarity': cos_sim(v1, v2)}
+
+
+@app.route('/put', methods=['put'])
+def put_test():
+    json = request.get_json()
+    print('==request==')
+    print(json)
+    print('===========')
+
 
 def get_vector(text):
     sum_vec = np.zeros(300)
@@ -36,9 +45,11 @@ def get_vector(text):
         node = node.next
     return sum_vec / word_count
 
+
 # cos類似度を計算
 def cos_sim(v1, v2):
     return np.dot(v1, v2) / (np.linalg.norm(v1) * np.linalg.norm(v2))
+
 
 # メイン関数
 if __name__ == '__main__':
